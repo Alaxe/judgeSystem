fafccdd7a5cec1d02ba5a67f26e726ca39f0066e
Alexander Crustev <aleks.tcr@gmail.com>
2015-10-03 16:11:52 +0300

    Blog admin panel
     - Added buttons for admin controll in the blog app

--- blog/templatetags/blog.py
+++ blog/templatetags/blog.py
@@ -3,6 +3,18 @@ from django.utils import timezone
 
 register = template.Library()
 
+@register.inclusion_tag('blog/post_admin_panel.html', takes_context = True)
+def post_admin_panel(context, *args, **kwargs):
+    user = context.request.user
+
+    templateContext = {}
+    if 'post' in kwargs:
+        templateContext['has_permission'] = user.has_perm('blog.change_post')
+        templateContext['post'] = kwargs['post']
+    else:
+        templateContext['has_permission'] = user.has_perm('blog.create_post')
+    return templateContext
+
 @register.filter
 def post_subheading(post):
     timeStr = post.post_time.strftime('%d %b %Y')
